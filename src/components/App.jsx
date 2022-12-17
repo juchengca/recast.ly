88eb54f2b34f74f83eecd5b13560f3e23987f987
Justin Cheng <jucheng.ca@gmail.com>
2022-12-17 11:18:59 -0800

    Complete creating reusable API helper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import VideoList from './VideoList.js';
 import VideoListEntry from './VideoListEntry.js';
 import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
+import Search from './Search.js';
 
 var App = () => {
 // class App extends React.Component {
@@ -14,7 +15,8 @@ var App = () => {
     <div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          {/*<div><h5><em>search</em> view goes here</h5></div>*/}
+          <Search />
         </div>
       </nav>
       <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -6,6 +6,20 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 
 var searchYouTube = (query, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: {q: query},
+    dataType: 'json', // added data type
+    youtube_api_key: YOUTUBE_API_KEY,
+    success: function(res) {
+      callback(res);
+      console.log('successfully searched YouTube');
+    },
+    error: function() {
+      console.log('search YouTube error');
+    }
+  });
 };
 
 export default searchYouTube;
